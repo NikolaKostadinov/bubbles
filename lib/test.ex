6ee8eabda089b8a7126c57850d53fd8808400e31
Nikola Kostadinov <nikolakostadinov@protonmail.com>
2022-12-11 16:46:37 +0200

    communication fixed

--- lib/test.ex
+++ lib/test.ex
@@ -3,8 +3,14 @@ defmodule Test do
   @generic_name :ivcho
   @generic_pass "123456"
 
-  def user(username \\ @generic_name) do
+  def user(username \\ @generic_name) when is_atom(username) do
     %UserStruct{ username: username, password: @generic_pass }
+  end
+
+  def process(username) when is_atom(username) do
+    username
+      |> user()
+      |> process()
   end
 
   def process(user) do
