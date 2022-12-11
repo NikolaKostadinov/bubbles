8c709fa2ef2d3787e01543a92debaa1abc461202
Nikola Kostadinov <nikolakostadinov@protonmail.com>
2022-12-11 12:43:50 +0200

    messages

--- lib/message_struct.ex
+++ lib/message_struct.ex
@@ -7,11 +7,26 @@ defmodule MessageStruct do
   """
 
   defstruct [
-    from:   nil,
-    to:     nil,
-    value:   ""
+    from:   self(),
+    to:        nil,
+    value:      ""
   ]
 
+  @doc """
+    Guard checks if message is a valid `MessageStruct`.
+  """
+  defguard is_message(message)          when
+    message.__struct__ === MessageStruct and
+    is_pid(message.from)                 and
+    is_pid(message.to)                   and
+    is_binary(message.value)             and
+    message.to    !== message.from       and
+    message.value !== ""
 
+  @doc """
+    Check if `MessageStruct` is valid
+  """
+  def valid_message?( message) when is_message(message) do true  end
+  def valid_message?(_message)                          do false end
 
 end
