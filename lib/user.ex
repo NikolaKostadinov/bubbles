3183f7fd40203dda0f9002ecf02e5a1f5d9db158
Nikola Kostadinov <nikolakostadinov@protonmail.com>
2022-12-11 13:46:09 +0200

    friends forever

--- lib/user.ex
+++ lib/user.ex
@@ -7,7 +7,7 @@ defmodule User do
     Start a `User` process with initial `UserStruct`.
   """
   def start(user) when is_user(user) do
-    GenServer.start(__MODULE__, user, name: __MODULE__)
+    GenServer.start(__MODULE__, user)
   end
 
   @doc """
@@ -17,6 +17,17 @@ defmodule User do
     GenServer.cast(pid, :inspect)
   end
 
+  @doc """
+    Befriend two `User` processes,
+    so they can communicate.
+  """
+  def befriend(this_pid, other_pid) when is_pid(this_pid) and is_pid(other_pid)  do
+
+    GenServer.cast( this_pid, { :befriend, other_pid })
+    GenServer.cast(other_pid, { :befriend,  this_pid })
+
+  end
+
   @impl true
   def init(user) do
     { :ok, %UserStruct{ user | id: self() } }
@@ -24,8 +35,16 @@ defmodule User do
 
   @impl true
   def handle_cast(:inspect, state) do
-    IO.inspect(state)
+    IO.inspect( state )
     { :noreply, state }
+  end
+
+  @impl true
+  def handle_cast({ :befriend, pid }, state) do
+    state
+    |> UserStruct.befriend(pid)
+    |> Enum.uniq()
+    |> (&{ :noreply, &1 }).()
   end
 
 end
--- lib/user_struct.ex
+++ lib/user_struct.ex
@@ -19,17 +19,10 @@ defmodule UserStruct do
   defguard is_user(user)              when
     user.__struct__ === UserStruct     and
     is_pid(user.id) or user.id === nil and
+    is_atom(user.username)             and
     is_list(user.friends)              and
     is_list(user.messages)             and
-    user.username !== nil              and
     user.password !== nil
-
-  @doc """
-    Guard checks if **two** users are valid.
-    This is used to save some space in code.
-  """
-  defguard are_users(this_user, other_user) when
-    is_user(this_user) and is_user(other_user)
 
   @doc """
     Check if `UserStruct` is valid
@@ -39,22 +32,22 @@ defmodule UserStruct do
 
   @doc """
     This functions is the definition of
-    love and joy. It befirends the *second*
-    user to the *first*.
+    love and joy. It befirends the pid of
+    the *second* user to the state of the *first*.
   """
-  def befriend(this_user, other_user) when are_users(this_user, other_user) do
-    new_friends = [ other_user.id | this_user.friends ]
-    %UserStruct{ this_user | friends: new_friends }
+  def befriend(user, pid) when is_user(user) and is_pid(pid) do
+    new_friends = [ pid | user.friends ]
+    %UserStruct{ user | friends: new_friends }
   end
 
   @doc """
     This functions is the definition of
-    hate. It defirends the *second*
-    user from the *first*.
+    hate. It defirends the pid of the
+    *second* user from the state of the *first*.
   """
-  def defriend(this_user, other_user) when are_users(this_user, other_user) do
-    new_friends = List.delete(this_user.friends, other_user.id)
-    %UserStruct{ this_user | friends: new_friends }
+  def defriend(user, pid) when is_user(user) and is_pid(pid) do
+    new_friends = List.delete(user.friends, pid)
+    %UserStruct{ user | friends: new_friends }
   end
 
 end
